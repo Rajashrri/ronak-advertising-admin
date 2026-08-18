da2b7d26d4e283ffffe8c0017f8b3842123a7606
Devendramali <devendramali01234@gmail.com>
2026-08-18 18:54:57 +0530

    new chnages

--- src/pages/AuthPages/ResetPasswordPage.tsx
+++ src/pages/AuthPages/ResetPasswordPage.tsx
@@ -7,8 +7,8 @@ export default function ResetPasswordPage() {
   return (
     <>
       <PageMeta
-        title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+       title="ronak-advertising"
+        description="Build a bigger presence Across the city"
       />
       <AuthLayout>
         <ResetPassword />
